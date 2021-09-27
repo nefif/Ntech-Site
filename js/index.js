f2adfcd110a769998e4d27cffd61b2023b6236de
Néfi Fernandes <nefifernandes14@yahoo.com.br>
2021-09-27 16:50:56 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,31 @@
+/* jQuery(document).ready(function ($) {
+    //FIXED HEADER
+    window.onscroll = function () {
+      if (window.pageYOffset > 140) {
+        $("#header").addClass("active");
+      } else {
+        $("#header").removeClass("active");
+      }
+    }
+}); */
+
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 30,
+  autoplay: true,
+  autoplayTimeout: 2,
+  dots: true,
+  lazyLoad: true,
+  nav: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 4,
+    },
+  },
+});
